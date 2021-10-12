91b2ad0074c2a68b13ed74a59d634eb512178684
Andrew Tran <abtran@gmail.com>
2021-10-12 19:55:41 -0400

    exercises

---
+++ chj-day2-p1/exercises_d1p1_questions.R
@@ -0,0 +1,47 @@
+# Exercises
+
+# 1. import data from chj-day2-p1/data/
+# file name: All Maritime I-D reports 1.1 - 4.30 - REDACTED.xlsx
+
+# Q1. What two packages do you need to load?
+# Go ahead and load them
+#--------------------------------
+
+
+# Q1: Load the xlsx spreadsheet into an object called "reports"
+# do you remember what function to use?
+#-------------------------------- 
+
+
+
+# Q2: How many rows is it?
+#-------------------------------- 
+
+
+
+# Q3: Create a new dataframe called "LA" with people who were at QStation of Los Angeles
+# How many rows is it? 
+#-------------------------------- 
+
+
+# Q4: Can you narrow the LA dataframe from 100 colums to the columns:
+# ReportDate, DeathIllness, NotificationTime, Agency, PersonType, and Gender?
+#-------------------------------- 
+
+# Q5: What's the earliest date someone from QStation Los Angeles became sick?
+#-------------------------------- 
+
+
+
+# Q6: What's the latest date someone from QStation Los Angeles became sick in the data?
+#-------------------------------- 
+
+
+# Q7: Create a new dataframe called "crew" with people who were a Crew Member (PersonType) 
+# How many rows is it? 
+#-------------------------------- 
+
+
+# Q8: What's the percent of crew to everyone else who got sick?
+#-------------------------------- 
+
